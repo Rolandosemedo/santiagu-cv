390e1932205ef4cb7c72aa3c8771ba6870e5e768
Rolando Semedo <r9msemedo@gmail.com>
2026-05-19 02:34:16 +0100

    feat: mapa fullscreen apenas OpenStreetMap

--- apps/web/src/app/mapa/page.tsx
+++ apps/web/src/app/mapa/page.tsx
@@ -1,7 +1,5 @@
 import { MapClient } from "./MapClient";
-import { fetchPlaces } from "@/lib/api";
 export const dynamic = "force-dynamic";
-export default async function MapaPage() {
-  const { data: places } = await fetchPlaces({ limit: 200 });
-  return <MapClient places={places} />;
+export default function MapaPage() {
+  return <MapClient />;
 }
